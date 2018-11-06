c85d63c231a20bf3d9e8b4908e5bc4c7bfcf289d
Ethan Simpson <ethansown@gmail.com>
2018-11-05 22:10:26 -0600

    Created Array to track all Cell Positions

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
 var allRows = document.getElementsByClassName("row");
 console.log(allRows);
 var cells = document.getElementsByClassName("cell");
-var cellsArray = [];
-for(i = 0; i < cells.length; i++){
-    cellsArray.push(cells[i]);
-}
-console.log(cellsArray);
 var colorPicker = document.querySelector('#colorPicker');
 var defaultColor = '#000000';
 colorPicker.value = defaultColor;
 console.log(colorPicker.value);
 var clearCellsButton = document.getElementById('clearCells');
 
-/*var cellsCountArray = new Array(25);
-for(i = 0; i < 25; i++){
-    cellsArray[i] = new Array(20);
-    for(j = 0; j < 20; j++){
-        cellsArray[i][j] = cells[j];
-        
-    }
-}
-console.log(cellsArray);
-*/
-
-
-window.onload = function cellsOnLoad(){
+function cellsOnLoad(){
     const cellsContainer = document.getElementById('cells-container');
     for(i = 0; i < 26; i++){
         cellsContainer.insertAdjacentHTML('beforeend','<div class="row"> <div class="cell"></div> <div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div><div class="cell"></div></div>' );
     }
 }
+window.addEventListener('load', cellsOnLoad());
 function setCells(){
 for (i = 0; i < cells.length; i++){
     cells[i].setAttribute('onmouseover', 'colorThis(this)');
@@ -48,3 +32,24 @@ function clearCells(){
 function colorThis(cell){
     cell.style.backgroundColor = colorPicker.value;
 }
+function buildArray() {
+    const
+      rows = Array
+        .from(document.querySelectorAll('#cells-container .row'))
+        .reverse(),
+      x = rows[0].children.length,
+      y = rows.length,
+      thisArray = [];
+  
+  
+    for (let i=0; i<x; i++) {
+      thisArray[i] = new Array(y);
+      for (let j=0; j<y; j++) {
+        thisArray[i][j] = rows[j].children[i];
+      }
+    }
+  
+    return thisArray;
+  }
+  
+  let cellsArray = buildArray();
